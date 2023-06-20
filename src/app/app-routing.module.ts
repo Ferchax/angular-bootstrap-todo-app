b9a951fc0645daf749040c7dd30018e9b8d0e93b
Ferchax <fgc.schlottmann@gmail.com>
2023-06-19 22:10:17 -0300

    modify header and footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,9 +6,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { NotesComponent } from './pages/notes/notes.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'notes', component: NotesComponent},
   { path: 'about', component: AboutComponent},
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }
